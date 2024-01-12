0a8f8697ddc5dd0ff8a671f27d303bd8df7f4c1a
gianbiscotto <68618077+gianbiscotto@users.noreply.github.com>
2024-01-12 12:32:15 +0100

    Delete Assets directory

--- Assets/Scenes/Script/NewBehaviourScript.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7f43720999e04865485278c2cc0b95e5f4b13288c9961d098ce19b05503c6005
-size 324
--- Assets/X.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a3dbea2c816a6aafb4293fbca32ce5ed43248907f7cb586787f70cfcac39809f
-size 364
